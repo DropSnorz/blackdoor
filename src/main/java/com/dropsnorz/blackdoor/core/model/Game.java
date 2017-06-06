b6a3e7b367ccfb0e923a260ef7bfd94f7069d0c5
Arthur POIRET <dropsnorz@gmail.com>
2017-06-06 17:46:13 +0000

    Filled level 4

--- src/main/java/com/dropsnorz/blackdoor/core/model/Game.java
+++ src/main/java/com/dropsnorz/blackdoor/core/model/Game.java
@@ -55,6 +55,12 @@ public class Game {
 		fragmentsManager.addFragment(new CodeFragment("GET_INSTALLED_APP", "getInstalledApplications()"));
 		fragmentsManager.addFragment(new CodeFragment("VAR_MANAGER", "manager", CodeFragmentType.TYPE_VARIABLE));
 
+		//Level 4
+		
+		fragmentsManager.addFragment(new CodeFragment("CURSOR_CURSOR", "Cursor cursor", CodeFragmentType.TYPE_VARIABLE));
+		fragmentsManager.addFragment(new CodeFragment("RETURN_CURSOR_SEMICOLON", "return cursor;", CodeFragmentType.TYPE_VARIABLE));
+		fragmentsManager.addFragment(new CodeFragment("GET_CONTENT_RESOLVER", "getContentResolver()"));
+		fragmentsManager.addFragment(new CodeFragment("QUERY_CONTENT_CALLS", "query(content://calls/lastCalls)"));
 		
 		GameLevel l1 = new GameLevel("level1", this);
 		l1.setTitle("Mission 1");
@@ -105,7 +111,7 @@ public class Game {
 		GameLevel l3 = new GameLevel("level3", this);
 		l3.setTitle("Mission 3");
 		l3.setIntroText("« Bon voyons un peu ce qu'il y'a sur ce téléphone. Est-ce que tu peux obtenir la liste des applications installées sur son terminal ? On aura un bon point du vue sur les réseaux sociaux qu'il peut utiliser pour communiquer avec d'éventuels complices »");
-		l3.setHelperText("Retourner la liste des applications instéllées sur le téléphone");
+		l3.setHelperText("Retourner la liste des applications installées sur le téléphone");
 		
 		l3.getFragmentList().add(fragmentsManager.getFragmentById("PACKAGE_MANAGER_MANAGER"));
 		l3.getFragmentList().add(fragmentsManager.getFragmentById("GET_SYSTEM_SERVICE_TELEPHONY"));
@@ -127,9 +133,37 @@ public class Game {
 		
 		l3.getDialogKeywordFinder().addKeywordMapping(null, new Dialog("Continuer"));
 		
+		
+		
+		GameLevel l4 = new GameLevel("level4", this);
+		l4.setTitle("Mission 4");
+		l4.setIntroText("« Je ne veux pas te mettre la pression mais le reste de l'équipe est très inquiète a propos de cet individu. Nous allons avoir besoin d'informations plus probantes. Essaye d'obtenir la liste de ses derniers appels. »");
+		l4.setHelperText("Retourner la liste des dernier appels");
+		
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("CURSOR_CURSOR"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("RETURN_CURSOR_SEMICOLON"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("QUERY_CONTENT_CALLS"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("GET_CONTENT_RESOLVER"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("GET_CONTEXT()"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("OP_DOT"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("OP_EQUALS"));
+		l4.getFragmentList().add(fragmentsManager.getFragmentById("OP_SEMICOLON"));
+		
+		
+		builder = new FragmentListBuilder(fragmentsManager);
+		builder.add("CURSOR_CURSOR").add("OP_EQUALS").add("GET_CONTEXT()").add("OP_DOT").add("GET_CONTENT_RESOLVER").add("OP_DOT").add("QUERY_CONTENT_CALLS").add("OP_SEMICOLON")
+			.add("RETURN_CURSOR_SEMICOLON");
+		
+		l4.setResponseFragmentList(builder.build());
+		
+		l4.getDialogKeywordFinder().addKeywordMapping(null, new Dialog("Continuer"));
+		
+		
+		
 		gameLevelList.add(l1);
 		gameLevelList.add(l2);
 		gameLevelList.add(l3);
+		gameLevelList.add(l4);
 		
 		currentGameLevel = l1;
 		
